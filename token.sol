2531f5993c30a8854d317f8214ecc36c7454c482
Štěpán Fau <sbren@Hexsus1.local>
2021-05-22 12:27:32 +0200

    initial commit

---
+++ token.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.8.0;
+
+interface IERC20 {
+    // Create a standard ERC-20 interface
+}
+
+// EBT = ERC20 Basic Token
+// Basic token contract solely for the purpose of learning Solidity & practice
+// @dev Current assumption is that safemath library is no longer needed as solidity v8 and above checks for overflows by default
+
+contract EBT is IERC20 {
+    // Implement functions according to IERC20 interface
+}
